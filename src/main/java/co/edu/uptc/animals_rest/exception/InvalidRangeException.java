efcaf556bced8cf7437b560c7ffb57c645fe4779
la00429 <laura.figuereedo02@uptc.edu.co>
2024-09-22 23:33:43 -0500

    Eliminando src

--- src/main/java/co/edu/uptc/animals_rest/exception/InvalidRangeException.java
+++
@@ -1,9 +0,0 @@
-package co.edu.uptc.animals_rest.exception;
-
-
-public class InvalidRangeException extends RuntimeException {
-
-    public InvalidRangeException(String message) {
-        super(message);
-    }
-}
